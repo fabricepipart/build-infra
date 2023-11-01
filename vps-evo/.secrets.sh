3705a93521f7e0340bddbd518d4f3c4eb136a654
Fabrice Pipart <fabrice.pipart@gmail.com>
2023-11-01 12:17:29 +0100

    Debug

--- vps-evo/.secrets.sh
+++ vps-evo/.secrets.sh
@@ -1,22 +1,18 @@
 #!/usr/bin/env bash
 
 TORRENTS_USER=USER
-TORRENTS_PASSWORD=PASSWORD
+TORRENTS_PASSWORD=ASSWORD
 
 
 
 
 
-
-
-
-
-
-
-if cat $0 | grep '[A-Z_-]\+=[A-Z_-]\+' | cut -d'=' -f2 | grep 'PASSWORD' >& /dev/null
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+MYSELF=$SCRIPT_DIR/.secrets.sh
+if cat $MYSELF | grep '[A-Z_-]\+=[A-Z_-]\+' | cut -d'=' -f2 | grep 'PASSWORD' >& /dev/null
 then
-    echo "Please modify $0 so that it contains real passwords"
-    false
+    echo "Please modify $MYSELF so that it contains real passwords"
+    exit
 else
     echo "Password variables loaded"
 fi
