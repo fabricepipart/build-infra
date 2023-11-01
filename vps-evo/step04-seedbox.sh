d787e4a7a4eae9e53c142758b27c31b69fd7f8cd
Fabrice Pipart <fabrice.pipart@gmail.com>
2023-11-01 13:59:18 +0100

    webdav

--- vps-evo/step04-seedbox.sh
+++ vps-evo/step04-seedbox.sh
@@ -8,3 +8,10 @@ source $(dirname -- "$0")/.secrets.sh
 
 podname=$(kubectl get po -n seedbox -o name | grep rtorrent)
 kubectl exec -it $podname -n seedbox -- bash -c "echo '"$TORRENTS_USER":$(openssl passwd -1 "$TORRENTS_PASSWORD")' > /downloads/.htpasswd"
+
+TORRENTS_HTPWD=$(htpasswd -n -b $TORRENTS_USER $TORRENTS_PASSWORD)
+kubectl create secret -n seedbox generic seedbox-credentials \
+    --from-literal=TORRENTS_USER=$TORRENTS_USER \
+    --from-literal=TORRENTS_PASSWORD=$TORRENTS_PASSWORD \
+    --from-literal=TORRENTS_HTPWD=$TORRENTS_HTPWD
+
